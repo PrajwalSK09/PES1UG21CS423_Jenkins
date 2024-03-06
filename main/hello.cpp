cdcfd0c965330729813a35e30c6c79e451fb4585
PrajwalSK09 <147812089+PrajwalSK09@users.noreply.github.com>
2024-03-06 18:36:55 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout<<"PES1UG21CS423.\n";
+  cout<<"Hello PESU\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
